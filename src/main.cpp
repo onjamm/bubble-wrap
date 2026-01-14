d49bda3e58704d1eafa2b0e98a82b905fc8ca130
onjamm <75151498+onjamm@users.noreply.github.com>
2026-01-14 22:34:41 +0000

    Added A + B color changing functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 
 int main() {
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(20, 20, 31));
+    bn::backdrop::set_color(bn::color(20, 0, 20));
 
     while(true) {
         bn::core::update();
+
+        if (bn::keypad::a_pressed()) {
+        bn::backdrop::set_color(bn::color(31, 21, 22));
+    }
+
+        if (bn::keypad::b_pressed()) {
+            bn::backdrop::set_color(bn::color(0, 0, 30));
+        }
+
     }
 }
