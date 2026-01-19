973ba45107864333e0b3637a32fed2ed92131104
onjamm <75151498+onjamm@users.noreply.github.com>
2026-01-19 22:50:21 +0000

    Added my art for the world to see!

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,28 @@
 #include <bn_color.h>
 #include <bn_core.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 
 int main() {
     bn::core::init();
 
     bn::backdrop::set_color(bn::color(20, 0, 20));
+
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-20, -10);
+
+    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(20, -10);
+
+    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(15, 30);
+
+     bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(0, 45);
+
+    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(10, 40);
+
+    bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-15, 30);
+
+    bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(-10, 40);
+   
 
     while(true) {
         bn::core::update();
