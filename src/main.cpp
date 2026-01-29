75c4408eb7b9d34416e0046040eca54f07b330c7
onjamm <75151498+onjamm@users.noreply.github.com>
2026-01-29 01:51:30 +0000

    Did transformation assignment, I probably should have commited more, added personal touch if you click up or down the first sprite circle[0] now moves up or down respectively

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,25 @@ int main() {
         bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(x, 40);
         circles.push_back(myCircle);
     }
+    circles[0].set_scale(1.8);
+    circles[1].set_scale(0.5);
+    circles[2].set_scale(2.0);
+    circles[3].set_scale(0.01);
+    circles[4].set_scale(2);
+    circles[4].set_horizontal_scale(0.5);
+    circles[5].set_vertical_scale(1.1);
+    circles[5].set_shear(0.2);
+    
+
+    //Vector with more than 32 items, when I ran it said no more affine sprites 
+    // bn::vector<bn::sprite_ptr, 33> circles2 = {};
+    // for (int i = 0; i < 33; ++i) {
+    //     bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(i, -20);
+    //     myCircle2.set_scale(0.5);
+    //     circles2.push_back(myCircle2);
+    // }
+
+    //interstingly if scale values are "too much" the circles become squares / or rectangles
 
     while(true) {
         bn::core::update();
@@ -60,5 +79,13 @@ int main() {
         if(bn::keypad::a_pressed() & bn::keypad::b_pressed()) {
             bn::backdrop::set_color(bn::color(20, 0, 20));
         } 
+
+        //creative change for Sprite Transformation assignment if you press up or down the first sprite circle[0] will move up or down respectively
+        if(bn::keypad::up_pressed()) {
+            circles[0].set_y(circles[0].y() - bn::fixed(1.2));
+        }
+        if(bn::keypad::down_pressed()) {
+            circles[0].set_y(circles[0].y() + bn::fixed(1.2));
+        }
     }
 }
